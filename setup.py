cbbef96c60eb3c50c65039018e4141dfc548bb0c
Nasir Shadravan <nasir@semicolon.io>
2023-02-09 11:12:45 +0100

    Update with some code

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 from packagename.version import Version
 
 
-setuptools.setup(name='py-boilerplate',
+setuptools.setup(name='dagger',
                  version=Version('1.0.0').number,
-                 description='Python Package Boilerplate',
+                 description='Sample Dagger Project',
                  long_description=open('README.md').read().strip(),
-                 author='Package Author',
+                 author='Nasir SHadravan',
                  author_email='you@youremail.com',
                  url='http://path-to-my-packagename',
-                 py_modules=['packagename'],
+                 py_modules=['packagename', 'dags'],
                  install_requires=[],
                  license='MIT License',
                  zip_safe=False,
